8950a63a764f409e65587020f3f6962a08fe2740
钱昱冰 <qyb@bytedance.com>
2025-06-23 11:33:05 +0800

    update kfac_jax to 0.0.7

--- deephall/optimizers/kfac.py
+++ deephall/optimizers/kfac.py
@@ -16,8 +16,12 @@
 # ("Bytedance's Modifications"). All Bytedance's Modifications are
 # Copyright 2024-2025 Bytedance Ltd. and/or its affiliates.
 
-from collections.abc import Mapping, Sequence
-from typing import Any, cast
+import dataclasses
+from collections.abc import Sequence
+from functools import partial
+from math import prod
+from string import ascii_lowercase
+from typing import cast
 
 import jax
 import kfac_jax
@@ -30,74 +34,94 @@ from deephall.log import CheckpointState
 from deephall.loss import LossStats
 from deephall.types import TrainingInit, TrainingStep
 
-repeated_dense_tag = kfac_jax.LayerTag("repeated_dense", num_inputs=1, num_outputs=1)
-
-
-def register_repeated_dense(y, x, w, b=None):
-    if b is None:
-        return repeated_dense_tag.bind(y, x, w)
-    return repeated_dense_tag.bind(y, x, w, b)
-
 
 class RepeatedDenseBlock(kfac_jax.DenseTwoKroneckerFactored):
-    """Dense block that is repeatedly applied to multiple inputs (e.g. vmap)."""
+    """Dense block that is repeatedly applied to multiple inputs (e.g. vmap).
 
-    def parameters_shaped_list_to_array(
-        self,
-        parameters_shaped_list: Sequence[kfac_jax.utils.Array],
-    ) -> kfac_jax.utils.Array:
-        for p, s in zip(parameters_shaped_list, self.parameters_shapes):
-            assert p.shape == s
+    By default, kfac_jax will assume that the blocks only transforms the last axis,
+    i.e. `ij,jk->ik`. However, in general, the dense block ca be repeatedly applied
+    (i.e. transforming the last axis and keeping all other axis onchanged), or we can
+    transform more axis, and the output shape is not necessarily the same as the input.
+    Therefore, we need to modify the ways to handle the parameters.
+    """
 
-        if self.has_bias:
-            w, b = parameters_shaped_list
-            return jnp.concatenate(
-                [w.reshape([-1, b.size]), b.reshape([1, -1])], axis=0
-            )
+    def __init__(self, layer_tag_eq):
+        # Even though the superclass constructor will set this later, we need to do
+        # it now since it's used below by `self.parameters_shapes`.
+        self._layer_tag_eq = layer_tag_eq
 
-        else:
-            [w] = parameters_shaped_list
-            return w.reshape([w.shape[0], -1])
+        parameters_specs = []
+        _, w_dim_out = self.weight_dimension_split
 
-    def array_to_parameters_shaped_list(
-        self, array: kfac_jax.utils.Array
-    ) -> tuple[kfac_jax.utils.Array, ...]:
-        if self.has_bias:
-            w, b = array[:-1], array[-1]
-            w_shape, b_shape = self.parameters_shapes
-            return w.reshape(w_shape), b.reshape(b_shape)
+        for shape in self.parameters_shapes:
+            in_str = ascii_lowercase[: len(shape)]
+            # Don't use `w_dim_in` because it's not applicable to bias
+            out_str = f"({in_str[:-w_dim_out]})({in_str[-w_dim_out:]})"
+            parameters_specs.append(f"{in_str} -> {out_str}")
 
-        else:
-            return tuple([array.reshape(self.parameters_shapes[0])])
+        super().__init__(layer_tag_eq, parameters_specs)
+
+    @property
+    def weight_dimension_split(self) -> tuple[int, int]:
+        """Determine the input/output dimensions transformed by the weight matrix.
+
+        The formula derives these values based on the input/output tensor dimensions
+        and the weight's shape to ensure proper contraction during matrix operations.
+
+        Returns:
+            - w_dim_in: Dimensions of the input's feature space transformed by weight.
+            - w_dim_out: Dimensions of the output's transformed feature space.
+        """
+        input_dim = len(self.inputs_shapes[0])
+        output_dim = len(self.outputs_shapes[0])
+        w_dim = len(self.parameters_shapes[0])
+        # No further assumption. It is equired for proper contraction
+        w_dim_in = (w_dim - (output_dim - input_dim)) // 2
+        w_dim_out = w_dim - w_dim_in
+        return w_dim_in, w_dim_out
 
     def fixed_scale(self) -> kfac_jax.utils.Numeric:
+        """A fixed scalar pre-factor of the curvature (e.g. constant)."""
         (x_shape,) = self.inputs_shapes
         return float(kfac_jax.utils.product(x_shape) // (x_shape[0] * x_shape[-1]))
 
     def update_curvature_matrix_estimate(
         self,
         state: kfac_jax.KroneckerFactored.State,  # type: ignore
-        estimation_data: Mapping[str, Sequence[kfac_jax.utils.Array]],
+        estimation_data: kfac_jax.LayerVjpData[kfac_jax.utils.Array],
         ema_old: kfac_jax.utils.Numeric,
         ema_new: kfac_jax.utils.Numeric,
+        identity_weight: kfac_jax.utils.Numeric,
         batch_size: kfac_jax.utils.Numeric,
     ) -> kfac_jax.KroneckerFactored.State:
-        [x] = estimation_data["inputs"]
-        [dy] = estimation_data["outputs_tangent"]
+        """Reshape the imputs and outputs before feeding them into parernt class."""
+        [x] = estimation_data.primals.inputs
+        [dy] = estimation_data.tangents.outputs
         assert x.shape[0] == batch_size
 
-        batch_size = x.size // (self.array_shape[0] - self.has_bias)
-        estimation_data = {
-            **estimation_data,
-            "inputs": (x.real.reshape((batch_size, -1)),),
-            "outputs_tangent": (dy.real.reshape((batch_size, -1)),),
-        }
+        w_dim_in, w_dim_out = self.weight_dimension_split
+        feature_size_in = prod(x.shape[-w_dim_in:])
+        feature_size_out = prod(dy.shape[-w_dim_out:])
 
+        estimation_data = dataclasses.replace(
+            estimation_data,
+            primals=dataclasses.replace(
+                estimation_data.primals,
+                inputs=(x.reshape([-1, feature_size_in]),),
+            ),
+            tangents=dataclasses.replace(
+                estimation_data.tangents,
+                outputs=(dy.reshape([-1, feature_size_out]),),
+            ),
+        )
+
+        batch_size = x.size // feature_size_in
         return super().update_curvature_matrix_estimate(
             state=state,
             estimation_data=estimation_data,
             ema_old=ema_old,
             ema_new=ema_new,
+            identity_weight=identity_weight,
             batch_size=batch_size,
         )
 
@@ -133,55 +157,51 @@ def _repeated_dense_complex_no_bias(
     return y
 
 
-def _dense_parameter_extractor(
-    eqns: Sequence[jax.core.JaxprEqn],
-) -> Mapping[str, Any]:
-    """Extracts all parameters from the conv_general_dilated operator."""
-    for eqn in eqns:
-        if eqn.primitive.name == "dot_general":
-            return dict(**eqn.params)
-    assert False
+_dense_parameter_extractor = partial(
+    kfac_jax.tag_graph_matcher._dense_parameter_extractor,  # pylint: disable=protected-access
+    variant="repeated_dense",
+)
 
 
 GRAPH_PATTERNS = (
     kfac_jax.tag_graph_matcher.GraphPattern(
         name="repeated_dense_with_bias",
-        tag_primitive=repeated_dense_tag,
+        tag_primitive=kfac_jax.layers_and_loss_tags.layer_tag,
         compute_func=_repeated_dense,
         parameters_extractor_func=_dense_parameter_extractor,
         example_args=[jnp.zeros([2, 3, 4]), [jnp.zeros([4, 3]), jnp.zeros([3])]],
     ),
     kfac_jax.tag_graph_matcher.GraphPattern(
         name="repeated_dense_no_bias",
-        tag_primitive=repeated_dense_tag,
+        tag_primitive=kfac_jax.layers_and_loss_tags.layer_tag,
         compute_func=_repeated_dense,
         parameters_extractor_func=_dense_parameter_extractor,
         example_args=[jnp.zeros([2, 3, 4]), [jnp.zeros([4, 3])]],
     ),
     kfac_jax.tag_graph_matcher.GraphPattern(
         name="repeated_dense_more_dim",
-        tag_primitive=repeated_dense_tag,
+        tag_primitive=kfac_jax.layers_and_loss_tags.layer_tag,
         compute_func=_repeated_dense,
         parameters_extractor_func=_dense_parameter_extractor,
         example_args=[jnp.zeros([1, 2, 3, 4]), [jnp.zeros([4, 3]), jnp.zeros(3)]],
     ),
     kfac_jax.tag_graph_matcher.GraphPattern(
         name="repeated_dense_more_dim_no_bias",
-        tag_primitive=repeated_dense_tag,
+        tag_primitive=kfac_jax.layers_and_loss_tags.layer_tag,
         compute_func=_repeated_dense,
         parameters_extractor_func=_dense_parameter_extractor,
         example_args=[jnp.zeros([1, 2, 3, 4]), [jnp.zeros([4, 3])]],
     ),
     kfac_jax.tag_graph_matcher.GraphPattern(
         name="repeated_dense_complex_no_bias",
-        tag_primitive=repeated_dense_tag,
+        tag_primitive=kfac_jax.layers_and_loss_tags.layer_tag,
         compute_func=_repeated_dense_complex_no_bias,
         parameters_extractor_func=_dense_parameter_extractor,
         example_args=[jnp.zeros([2, 3, 4], dtype=jnp.complex64), [jnp.zeros([4, 3])]],
     ),
     kfac_jax.tag_graph_matcher.GraphPattern(
         name="repeated_dense_attention_with_bias",
-        tag_primitive=repeated_dense_tag,
+        tag_primitive=kfac_jax.layers_and_loss_tags.layer_tag,
         compute_func=_repeated_dense_attention_out,
         parameters_extractor_func=_dense_parameter_extractor,
         example_args=[jnp.zeros([1, 2, 3, 4]), [jnp.zeros([3, 4, 3]), jnp.zeros([3])]],
